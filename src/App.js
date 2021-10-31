0861e334a8ca58e3845cf8af00de7837f3aa7c53
nmtty-nmtty <40553350+nmtty-nmtty@users.noreply.github.com>
2021-10-31 12:23:47 +0900

    select box map change test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import { useState } from "react";
 import "./styles.css";
 
-const InputText = () => {
-  const [inputTextValue, setInputTextValue] = useState("");
-  const [text, setText] = useState("JavaScript");
-  const handleChange = (e) => setInputTextValue(e.target.value);
+const InputSelectBox = () => {
+  const selectBoxArray = [
+    { id: 1, item: "HTML" },
+    { id: 2, item: "CSS" },
+    { id: 3, item: "JavaScript" }
+  ];
 
-  const handleClick = () => {
-    setText(inputTextValue);
-    setInputTextValue("");
-  };
+  const valse = selectBoxArray.map((vals) => (
+    <option value={vals.item} key={vals.id}>
+      {vals.item}
+    </option>
+  ));
+
+  const [selectedValue, setInputSelectedValue] = useState(valse[0]["item"]);
+  const handleChange = (e) => setInputSelectedValue(e.target.value);
 
   return (
     <div className="App">
-      <h1>I love {text}!</h1>
-      <input type="text" value={inputTextValue} onChange={handleChange} />
-      <input type="button" value="入力" onClick={handleClick} />
+      <p>
+        現在選択されている値：
+        <b>{selectedValue}</b>
+      </p>
+
+      <select value={selectedValue} onChange={handleChange}>
+        {valse}
+      </select>
     </div>
   );
 };
 
 export default function App() {
-  return <InputText />;
+  return <InputSelectBox />;
 }
