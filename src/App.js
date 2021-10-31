4a2b7049e0efda7e68bb73c6eee27e7bf80c9284
nmtty-nmtty <40553350+nmtty-nmtty@users.noreply.github.com>
2021-10-31 12:54:31 +0900

    radio button test

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
 import { useState } from "react";
 import "./styles.css";
 
-const InputSelectBox = () => {
-  const selectBoxArray = [
-    { id: 1, item: "HTML" },
-    { id: 2, item: "CSS" },
-    { id: 3, item: "JavaScript" }
-  ];
+const valuse = [
+  { id: 1, item: "赤" },
+  { id: 2, item: "青" },
+  { id: 3, item: "黒" }
+];
 
-  const valse = selectBoxArray.map((vals) => (
-    <option value={vals.item} key={vals.id}>
-      {vals.item}
-    </option>
-  ));
+const RadioBtnItems = ({ onChange, checked }) =>
+  valuse.map((value) => {
+    return (
+      <label key={value.id}>
+        <input
+          type="radio"
+          value={value.item}
+          onChange={onChange}
+          checked={checked === value.item}
+        />
+        {value.item}
+      </label>
+    );
+  });
 
-  const [selectedValue, setInputSelectedValue] = useState(valse[0]["item"]);
-  const handleChange = (e) => setInputSelectedValue(e.target.value);
+const InputRadio = () => {
+  const [checkedValue, setCheckedValue] = useState(valuse[0]["item"]);
+  const handleChange = (e) => setCheckedValue(e.target.value);
 
   return (
     <div className="App">
       <p>
         現在選択されている値：
-        <b>{selectedValue}</b>
+        <b>{checkedValue}</b>
       </p>
-
-      <select value={selectedValue} onChange={handleChange}>
-        {valse}
-      </select>
+      <RadioBtnItems onChange={handleChange} checked={checkedValue} />
     </div>
   );
 };
 
 export default function App() {
-  return <InputSelectBox />;
+  return <InputRadio />;
 }
